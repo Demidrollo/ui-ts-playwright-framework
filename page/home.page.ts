6fec0b1697249ed12b6c454543e4f2cca4db3a0f
Dmitry <demidrollo@gmail.com>
2025-03-12 21:18:23 +0100

    TASK-1: fix yml files

--- page/home.page.ts
+++ page/home.page.ts
@@ -3,7 +3,7 @@ import { BasePage } from './base.page';
 import { urlConfig } from '../config/env.config';
 
 export class HomePage extends BasePage {
-    constructor(page: Page) {
-        super(page, urlConfig.ui.homePage);
-    }
+  constructor(page: Page) {
+    super(page, urlConfig.ui.homePage);
+  }
 }
